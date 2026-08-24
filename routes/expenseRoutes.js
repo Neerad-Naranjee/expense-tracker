7d727e5e02b5631951f15ce3199d99c72a9be4c2
Neerad <neeradnaranjee7@gmail.com>
2026-08-24 16:51:19 -0400

    get method added

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const Expense = require('../models/Expense');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Expense Tracker API' });
+router.get("/", async (req, res) => {
+    try {
+        const expenses = await Expense.find();
+
+        res.json(expenses);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching expenses",
+            error: error.message
+        });
+    }
 });
 
 module.exports = router;
